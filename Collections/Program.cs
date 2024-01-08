3974305d4c98ca3f633f849df8c2cb79301ba6a1
Romain-GRADELET <romain.gradelet73@outlook.fr>
2024-01-08 13:09:47 +0100

    create outils.cs

--- Collections/Program.cs
+++ Collections/Program.cs
@@ -1,41 +1,10 @@
 ﻿using System;
+using OutilsCollections;
 
 namespace nombre_magique
 {
     class Program
     {
-        // Somme (int a
-        static int SommeTableaux(int[] t)
-        {
-            int somme = 0;
-
-            for (int i = 0; i < t.Length; i++)
-            {
-                somme += t[i];
-            }
-            return somme;
-        }
-
-        static void AfficherTableau(int[] tableau)
-        {
-            for (int i = 0;i < tableau.Length;i++)
-            {
-                Console.WriteLine("["+i+"] "+ tableau[i]);
-            }
-        }
-
-        static void AfficherValeurMaximal(int[] tableau) 
-        {
-            int maxValue = tableau.Max();
-            Console.WriteLine("La valeur maximale est: " + maxValue );
-        }
-
-        static void AfficherValeurMinimal(int[] tableau)
-        {
-            int maxValue = tableau.Min();
-            Console.WriteLine("La valeur maximale est: " + maxValue);
-        }
-
 
         static void Tableaux() 
         {
@@ -58,15 +27,15 @@ namespace nombre_magique
                 t[i] = rand.Next(0, 100);
             }
 
-            AfficherTableau (t);
-            AfficherValeurMaximal (t);
-            AfficherValeurMinimal (t);
+            outils.AfficherTableau (t);
+            outils.AfficherValeurMaximal (t);
+            outils.AfficherValeurMinimal (t);
 
             /*for (int i = 0; i < t.Length; i++)
             {
                 Console.WriteLine(t[i]);
             }
-            Console.WriteLine("Somme : " + SommeTableaux(t));*/
+            Console.WriteLine("Somme : " + outils.SommeTableaux(t));*/
         }
 
         static void Main(string[] args)
---
+++ Collections/outils.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OutilsCollections
+{
+    static class outils
+    {
+
+        public static int SommeTableaux(int[] t)
+        {
+            int somme = 0;
+
+            for (int i = 0; i < t.Length; i++)
+            {
+                somme += t[i];
+            }
+            return somme;
+        }
+
+        public static void AfficherTableau(int[] tableau)
+        {
+            for (int i = 0; i < tableau.Length; i++)
+            {
+                Console.WriteLine("[" + i + "] " + tableau[i]);
+            }
+        }
+
+        public static void AfficherValeurMaximal(int[] tableau)
+        {
+            int maxValue = tableau.Max();
+            Console.WriteLine("La valeur maximale est: " + maxValue);
+        }
+
+        public static void AfficherValeurMinimal(int[] tableau)
+        {
+            int maxValue = tableau.Min();
+            Console.WriteLine("La valeur maximale est: " + maxValue);
+        }
+
+    }
+}
